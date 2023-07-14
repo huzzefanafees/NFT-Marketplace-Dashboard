f0e0406d6768df3aa8b623c19aa77a57b12d3885
Huzzefa <huzzefanafees@gmail.com>
2023-07-14 19:46:28 +0500

    first commit

---
+++ chart.js
@@ -0,0 +1,73 @@
+let middleChart = document.getElementById('myChart');
+
+  let chart = new Chart(middleChart, {
+    type: 'line',
+    data: {
+      labels: ['0', '100', '80', '150', '125', '200', '120', '90', '150'],
+      datasets: [{
+        backgroundColor: "rgba(111, 79, 242, 0.3)",
+        borderColor: 'blue',
+        data: [0, 100, 80, 150, 125, 200, 120, 90, 150],
+        borderWidth: 3,
+        fill: true
+      }]
+    },
+    options: {
+        responsive: true,
+        scales: {
+                x: {
+                    display: false // No x-axis
+                },
+                y: {
+                    max: 350,
+                    grid: {
+                        display: false
+                     } // Set the maximum value on y-axis
+                }
+            },
+        plugins: {
+                legend: {
+                    display: false // No legend
+                }
+            }
+    }
+  });
+
+   // Chart Data
+   var chartData = {
+    labels: ['First Half', 'Second Half'],
+    datasets: [
+      {
+        data: [50, 50],
+        backgroundColor: ['#6F4FF2','rgba(111, 79, 242, 0.3)'],
+      }
+    ]
+  };
+
+  // Chart Options
+  var chartOptions = {
+    responsive: true,
+    cutout: "80%",
+    elements: {
+      arc: {
+        borderWidth: 2,
+        borderColor: '#fff'
+      }
+    },
+    tooltips: {
+      enabled: false
+    },
+    plugins: {
+        legend: {
+            display: false
+          }
+    }
+  };
+
+  // Create the doughnut chart
+  var ctx = document.getElementById('myChart1');
+  let chart1 = new Chart(ctx, {
+    type: 'doughnut',
+    data: chartData,
+    options: chartOptions
+  });
---
+++ script.js
@@ -0,0 +1,26 @@
+const tabs = document.querySelectorAll(".tab-btn");
+const content = document.querySelectorAll(".content");
+const theme1 = document.querySelector(".theme1");
+const theme2 = document.querySelector(".theme2");
+const themeIcon = document.querySelectorAll(".theme-toggler");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener('click', () => {
+    tabs.forEach(tab => {tab.classList.remove('active')});
+    tab.classList.add('active');
+
+    content.forEach(content => {content.classList.remove('active')});
+    content[index].classList.add('active');
+  })
+})
+theme1.addEventListener('click', () => {
+  document.body.classList.toggle("dark-theme-variables");
+  theme1.classList.remove('active');
+  theme2.classList.add('active');
+  theme2.classList.add('black');
+})
+theme2.addEventListener('click', () => {
+  document.body.classList.toggle("dark-theme-variables");
+  theme2.classList.remove('active')
+  theme1.classList.add('active')
+})
