b97cbecadf9e9006576c924a14cda0851b984ca8
Huzzefa <huzzefanafees@gmail.com>
2023-07-17 18:44:25 +0500

    Second Commit

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const themeIcon = document.querySelectorAll(".theme-toggler");
 
 tabs.forEach((tab, index) => {
   tab.addEventListener('click', () => {
-    tabs.forEach(tab => {tab.classList.remove('active')});
+    tabs.forEach(tab => { tab.classList.remove('active') });
     tab.classList.add('active');
 
-    content.forEach(content => {content.classList.remove('active')});
+    content.forEach(content => { content.classList.remove('active') });
     content[index].classList.add('active');
   })
 })
